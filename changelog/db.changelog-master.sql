4b274809944b762211795a6973d18936ab4c2c0c
raulsernadesangeles <rsernadesan@ucvvirtual.edu.pe>
2026-09-21 00:27:21 -0500

    Update db.changelog-master.sql

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
 
---changeset raulsernadesangeles:001 splitStatements:true
+--changeset raulsernadesangeles:001 splitStatements:true endDelimiter:;
 --validCheckSum: ANY
 CREATE SCHEMA IF NOT EXISTS ${CATALOG}.bi_lab_7003135940
 COMMENT 'Unity Catalog schema - BI and Big Data - Lab 02';
 
 --rollback DROP SCHEMA IF EXISTS ${CATALOG}.bi_lab_7003135940;
 
---changeset bryan:002 splitStatements:true
+--changeset bryan:002 splitStatements:true endDelimiter:;
 CREATE SCHEMA IF NOT EXISTS ${CATALOG}.bi_staging_7003135940
 COMMENT 'Staging schema - BI and Big Data - Lab 03';
 
