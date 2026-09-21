8a3ec4e0a74cbeff07ecfbf98b987db0e2e54bd0
raulsernadesangeles <rsernadesan@ucvvirtual.edu.pe>
2026-09-21 00:46:42 -0500

    Change schema creation to use workspace prefix
    Updated schema creation statements to use 'workspace' prefix.

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -1,15 +1,14 @@
 --liquibase formatted sql
---liquibase formatted sql parameterExpansion:true
 
 --changeset raulsernadesangeles:001 splitStatements:true endDelimiter:;
 --validCheckSum: ANY
-CREATE SCHEMA IF NOT EXISTS ${CATALOG}.bi_lab_7003135940
+CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_7003135940
 COMMENT 'Unity Catalog schema - BI and Big Data - Lab 02';
 
---rollback DROP SCHEMA IF EXISTS ${CATALOG}.bi_lab_7003135940;
+--rollback DROP SCHEMA IF EXISTS workspace.bi_lab_7003135940;
 
 --changeset bryan:002 splitStatements:true endDelimiter:;
-CREATE SCHEMA IF NOT EXISTS ${CATALOG}.bi_staging_7003135940
+CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_7003135940
 COMMENT 'Staging schema - BI and Big Data - Lab 03';
 
---rollback DROP SCHEMA IF EXISTS ${CATALOG}.bi_staging_7003135940;
+--rollback DROP SCHEMA IF EXISTS workspace.bi_staging_7003135940;
